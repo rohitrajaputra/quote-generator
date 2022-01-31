3f95be430ed03bf90f4b19df3b26045d887f9a06
Rohit Rajaputra <rohitsinghrajaputra@gmail.com>
2022-01-31 19:03:02 +0530

    Completed project

---
+++ src/components/Quotes.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Quotes = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [display, setDisplay] = useState();
+
+  const displayQuote = () => {
+    const data = quotes[Math.floor(Math.random() * quotes.length)];
+    if (data.author === null) {
+      data.author = "Unknown";
+    }
+    setDisplay(data);
+  };
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      const response = await axios.get("https://type.fit/api/quotes");
+      setQuotes(response.data);
+      setDisplay(response.data[0]);
+    };
+    getQuotes();
+  }, []);
+
+  const tweetquote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${display.text} - ${display.author}`;
+    window.open(twitterUrl, "_blank");
+  };
+
+  if (quotes.length > 0) {
+    return (
+      <div className="ui piled very padded segment">
+        <p style={{ fontSize: "30px", padding: "25px" }}>
+          {display ? display.text : ""}
+        </p>
+        <p style={{ fontSize: "20px" }}>{display ? display.author : ""}</p>
+        <div className="ui right floated button" onClick={displayQuote}>
+          Next
+        </div>
+        <div className="ui left floated blue button" onClick={tweetquote}>
+          <i className="twitter icon" />
+          Tweet
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="ui piled very padded loading segment">
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    );
+  }
+};
+
+export default Quotes;
